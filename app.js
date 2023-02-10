f5ad96330340821e82809b5c1d9ed81a1b97af19
cristi <scarlat.cristian.gabriel@gmail.com>
2023-02-10 22:22:54 +0200

    initial commit

---
+++ app.js
@@ -0,0 +1,13 @@
+"use strict";
+
+const openBtn = document.querySelector("#open");
+const closeBtn = document.querySelector("#close");
+const container = document.querySelector(".container");
+
+openBtn.addEventListener("click", () => {
+  container.classList.add("show-nav");
+});
+
+closeBtn.addEventListener("click", () => {
+  container.classList.remove("show-nav");
+});
